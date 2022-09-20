d13f125d3c76f74ce5da3c50b73f01ac8c573b05
bhargil <bhargil@einfotouch.com>
2022-09-20 21:51:02 +0530

    BJ - add new api

---
+++ api/src/controllers/users.js
@@ -0,0 +1,70 @@
+const CommonModel = require("../models/common");
+const config = require("./../config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require("uuid");
+
+exports.signIn = async (req, res, next) => {
+  try {
+    const postData = req.body;
+    if (!postData.email) {
+      res.status(401).json({ success: false, message: "Email is required." });
+      return;
+    }
+
+    if (!postData.password) {
+      res
+        .status(401)
+        .json({ success: false, message: "Password is required." });
+      return;
+    }
+
+    let getUser = await CommonModel.getRecords({
+      whereCon: [{ field: "email", value: postData.email }],
+      table: "users",
+      select: "*",
+    });
+
+    if (getUser.length) {
+      let isMatchedPassword = await bcrypt.compare(
+        postData.password,
+        getUser[0].password
+      );
+      if (isMatchedPassword) {
+        delete getUser[0].password;
+
+        const tokenUser = getUser[0];
+        const token = jwt.sign(tokenUser, config.secret, {
+          expiresIn: config.tokenLife,
+        });
+        var response = {
+          success: true,
+          message: "Login successfully",
+          user: tokenUser,
+          token: token,
+        };
+        res.status(201).json(response);
+      } else {
+        var response = {
+          success: false,
+          message: "Please enter correct password",
+        };
+        res.status(401).json(response);
+      }
+    } else {
+      res
+        .status(401)
+        .json({ success: false, message: "Please enter correct email." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+
+exports.fobiddenRoute = function (req, res, next) {
+  res.status(403).json({ message: "forbidden" });
+};
