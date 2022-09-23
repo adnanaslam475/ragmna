12e550617ea9e49accdbac59b0874678b11b6147
bhargil <bhargil@einfotouch.com>
2022-09-23 16:20:48 +0530

    BJ - all api for customer portal done

--- api/src/controllers/customer.js
+++ api/src/controllers/customer.js
@@ -3,7 +3,6 @@ const CommonModel = require("../models/common");
 //#region CREATE QUOTE
 exports.createUniqueQuote = async (req, res, next) => {
   try {
- 
     const postData = req.body;
     if (!postData.title) {
       res
@@ -30,6 +29,454 @@ exports.createUniqueQuote = async (req, res, next) => {
       res.status(201).json({
         success: false,
         message: "There is some problem, please try again later.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+//#endregion
+
+//#region Quote Personal Information
+
+exports.getPersonalInfo = async (req, res, next) => {
+  try {
+    if (!postData.quoteno) {
+      res
+        .status(201)
+        .json({ success: false, message: "Quote Number is required." });
+      return;
+    }
+    const whenCondtion = [];
+    whenCondtion.push({ field: "q.quote_number", value: postData.quoteno });
+
+    let result = await CommonModel.getRecords({
+      whereCon: whenCondtion,
+      table: "quote_personal_info qi",
+      select: "qi.*",
+      join: [
+        {
+          joinType: "INNER JOIN",
+          joinWith: "quote_master q",
+          joinCondition: "q.id = qi.fk_quote_id",
+        },
+      ],
+    });
+
+    res.status(201).json({
+      success: true,
+      items: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+
+exports.savePersonalInfo = async (req, res, next) => {
+  try {
+    const postData = req.body;
+
+    if (!postData.quoteno) {
+      res
+        .status(201)
+        .json({ success: false, message: "Quote Number is required." });
+      return;
+    }
+    if (!postData.fname) {
+      res
+        .status(201)
+        .json({ success: false, message: "First Name is required." });
+      return;
+    }
+    if (!postData.email) {
+      res.status(201).json({ success: false, message: "Email is required." });
+      return;
+    }
+    if (!postData.phone) {
+      res.status(201).json({ success: false, message: "Phone is required." });
+      return;
+    }
+    const whenCondtion = [];
+    whenCondtion.push({ field: "quote_number", value: postData.quoteno });
+
+    let getQuoteId = await CommonModel.getRecords({
+      whereCon: whenCondtion,
+      table: "quote_master",
+      select: "id",
+    });
+    console.log(getQuoteId);
+
+    let addData = await CommonModel.insertRecords(
+      {
+        fk_quote_id: getQuoteId[0]["id"],
+        fname: postData.fname,
+        lname: postData.lname,
+        email: postData.email,
+        contact_no: postData.phone,
+      },
+      "quote_personal_info"
+    );
+
+    if (addData) {
+      res.status(201).json({
+        success: true,
+        message: "Data saved successfully.",
+      });
+    } else {
+      res.status(201).json({
+        success: false,
+        message: "There is some problem, please try again later.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+
+exports.updatePersonalInfo = async (req, res, next) => {
+  try {
+    const postData = req.body;
+    if (!postData.fname) {
+      res
+        .status(201)
+        .json({ success: false, message: "First Name is required." });
+      return;
+    }
+    if (!postData.email) {
+      res.status(201).json({ success: false, message: "Email is required." });
+      return;
+    }
+    if (!postData.phone) {
+      res.status(201).json({ success: false, message: "Phone is required." });
+      return;
+    }
+
+    let updateData = {};
+    if (postData.fname) {
+      updateData.fname = postData.fname;
+    }
+    if (postData.lname) {
+      updateData.lname = postData.lname;
+    }
+    if (postData.email) {
+      updateData.email = postData.email;
+    }
+    if (postData.phone) {
+      updateData.contact_no = postData.phone;
+    }
+
+    let updatedDataResult = await CommonModel.updateRecords(
+      {
+        table: "quote_personal_info",
+        whereCon: [{ field: "id", value: postData.id }],
+      },
+      updateData
+    );
+
+    if (updatedDataResult) {
+      res.status(201).json({
+        success: true,
+        message: "Personal Info updated successfully.",
+      });
+    } else {
+      res.status(201).json({
+        success: true,
+        message: "Record are not available to update.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+//#endregion
+
+//#region Quote Company Information
+exports.getCompanyInfo = async (req, res, next) => {
+  try {
+    if (!postData.quoteno) {
+      res
+        .status(201)
+        .json({ success: false, message: "Quote Number is required." });
+      return;
+    }
+    const whenCondtion = [];
+    whenCondtion.push({ field: "q.quote_number", value: postData.quoteno });
+
+    let result = await CommonModel.getRecords({
+      whereCon: whenCondtion,
+      table: "quote_company_info qi",
+      select: "qi.*",
+      join: [
+        {
+          joinType: "INNER JOIN",
+          joinWith: "quote_master q",
+          joinCondition: "q.id = qi.fk_quote_id",
+        },
+      ],
+    });
+
+    res.status(201).json({
+      success: true,
+      items: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+
+exports.saveCompanyInfo = async (req, res, next) => {
+  try {
+    const postData = req.body;
+
+    if (!postData.quoteno) {
+      res
+        .status(201)
+        .json({ success: false, message: "Quote Number is required." });
+      return;
+    }
+    if (!postData.companyname) {
+      res
+        .status(201)
+        .json({ success: false, message: "Company Name is required." });
+      return;
+    }
+
+    const whenCondtion = [];
+    whenCondtion.push({ field: "quote_number", value: postData.quoteno });
+
+    let getQuoteId = await CommonModel.getRecords({
+      whereCon: whenCondtion,
+      table: "quote_master",
+      select: "id",
+    });
+
+    let addData = await CommonModel.insertRecords(
+      {
+        fk_quote_id: getQuoteId[0]["id"],
+        company_name: postData.companyname,
+        company_address: postData.companyaddress,
+        company_phone: postData.companyphone,
+      },
+      "quote_company_info"
+    );
+
+    if (addData) {
+      res.status(201).json({
+        success: true,
+        message: "Data saved successfully.",
+      });
+    } else {
+      res.status(201).json({
+        success: false,
+        message: "There is some problem, please try again later.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+
+exports.updateCompanyInfo = async (req, res, next) => {
+  try {
+    const postData = req.body;
+    if (!postData.companyname) {
+      res
+        .status(201)
+        .json({ success: false, message: "Company Name is required." });
+      return;
+    }
+
+    let updateData = {};
+    if (postData.companyname) {
+      updateData.company_name = postData.companyname;
+    }
+    if (postData.companyaddress) {
+      updateData.company_address = postData.companyaddress;
+    }
+    if (postData.companyphone) {
+      updateData.companyphone = postData.company_phone;
+    }
+
+    let updatedDataResult = await CommonModel.updateRecords(
+      {
+        table: "quote_company_info",
+        whereCon: [{ field: "id", value: postData.id }],
+      },
+      updateData
+    );
+
+    if (updatedDataResult) {
+      res.status(201).json({
+        success: true,
+        message: "Company Info updated successfully.",
+      });
+    } else {
+      res.status(201).json({
+        success: true,
+        message: "Record are not available to update.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+//#endregion
+
+//#region Quote Purpose Evalution
+exports.getPurposeInfo = async (req, res, next) => {
+  try {
+    if (!postData.quoteno) {
+      res
+        .status(201)
+        .json({ success: false, message: "Quote Number is required." });
+      return;
+    }
+    const whenCondtion = [];
+    whenCondtion.push({ field: "q.quote_number", value: postData.quoteno });
+
+    let result = await CommonModel.getRecords({
+      whereCon: whenCondtion,
+      table: "quote_eve_purpose qi",
+      select: "qi.*,pm.title as purposename, pm.title_ar as purposename_ar",
+      join: [
+        {
+          joinType: "INNER JOIN",
+          joinWith: "quote_master q",
+          joinCondition: "q.id = qi.fk_quote_id",
+        },
+        {
+          joinType: "INNER JOIN",
+          joinWith: "purpose_master pm",
+          joinCondition: "pm.id = qi.fk_purpose_id",
+        },
+      ],
+    });
+
+    res.status(201).json({
+      success: true,
+      items: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+
+exports.savePurposeInfo = async (req, res, next) => {
+  try {
+    const postData = req.body;
+
+    if (!postData.purposeid) {
+      res.status(201).json({ success: false, message: "Purpose is required." });
+      return;
+    }
+
+    const whenCondtion = [];
+    whenCondtion.push({ field: "quote_number", value: postData.quoteno });
+
+    let getQuoteId = await CommonModel.getRecords({
+      whereCon: whenCondtion,
+      table: "quote_master",
+      select: "id",
+    });
+
+    let addData = await CommonModel.insertRecords(
+      {
+        fk_quote_id: getQuoteId[0]["id"],
+        fk_purpose_id: postData.purposeid,
+        total_properties: postData.totalprop,
+        total_eva_need: postData.totalevalutor,
+      },
+      "quote_eve_purpose"
+    );
+
+    if (addData) {
+      res.status(201).json({
+        success: true,
+        message: "Data saved successfully.",
+      });
+    } else {
+      res.status(201).json({
+        success: false,
+        message: "There is some problem, please try again later.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      success: false,
+      message: "There is some problem, please try again later.",
+    });
+  }
+};
+
+exports.updatePurposeInfo = async (req, res, next) => {
+  try {
+    const postData = req.body;
+    if (!postData.purposeid) {
+      res.status(201).json({ success: false, message: "Purpose is required." });
+      return;
+    }
+
+    let updateData = {};
+    if (postData.purposeid) {
+      updateData.fk_purpose_id = postData.purposeid;
+    }
+    if (postData.totalprop) {
+      updateData.total_properties = postData.totalprop;
+    }
+    if (postData.totalevalutor) {
+      updateData.total_eva_need = postData.totalevalutor;
+    }
+
+    let updatedDataResult = await CommonModel.updateRecords(
+      {
+        table: "quote_eve_purpose",
+        whereCon: [{ field: "id", value: postData.id }],
+      },
+      updateData
+    );
+
+    if (updatedDataResult) {
+      res.status(201).json({
+        success: true,
+        message: "Data updated successfully.",
+      });
+    } else {
+      res.status(201).json({
+        success: true,
+        message: "Record are not available to update.",
       });
     }
   } catch (error) {
--- api/src/routes/customer.js
+++ api/src/routes/customer.js
@@ -3,6 +3,34 @@ const router = express.Router();
 const authUtil = require("../middlewares/authUtil.js");
 const customerController = require("../controllers/customer");
 
-/*Purpose ADD */
+/*New Quote ADD */
 router.post("/new-quote", customerController.createUniqueQuote);
+
+/* Get Personal Info */
+router.get("/personal-info", customerController.getPersonalInfo);
+
+/* Save Personal Info */
+router.post("/personal-info", customerController.savePersonalInfo);
+
+/* Update Personal Info */
+router.put("/personal-info", customerController.updatePersonalInfo);
+
+/* Get Company Info */
+router.get("/company-info", customerController.getCompanyInfo);
+
+/* Save Company Info */
+router.post("/company-info", customerController.saveCompanyInfo);
+
+/* Update Company Info */
+router.put("/company-info", customerController.updateCompanyInfo);
+
+/* Get Purpose Info */
+router.get("/purpose-eve-info", customerController.getPurposeInfo);
+
+/* Save Purpose Info */
+router.post("/purpose-eve-info", customerController.savePurposeInfo);
+
+/* Update Purpose Info */
+router.put("/purpose-eve-info", customerController.updatePurposeInfo);
+
 module.exports = router;
